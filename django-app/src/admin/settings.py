0e3741701b85a82e26b443f2e1107b0e97d3c992
Majd Saleh <mutairimjd@gmail.com>
2020-12-14 21:28:49 +0300

    User model creation and migration + built docker images for api and DB + config the docker-compose file

--- django-app/src/admin/settings.py
+++ django-app/src/admin/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'admin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_admin',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'admin_db',
+        'PORT': '3306',
     }
 }
 
